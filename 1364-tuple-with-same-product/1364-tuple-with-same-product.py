c91cc3e84850ebba58e793eaf5d236d1471272a3
Shyam Agrawal <105172277+iamshyamgoyal@users.noreply.github.com>
2025-02-06 08:46:38 +0530

    Time: 351 ms (67.77%), Space: 46.3 MB (65.4%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.py
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -5,16 +5,14 @@ class Solution:
         product_count = defaultdict(int)
         n = len(nums)
         
-        # Count occurrences of each product
         for i in range(n):
             for j in range(i + 1, n):
                 product = nums[i] * nums[j]
                 product_count[product] += 1
         
         count = 0
-        # Calculate number of valid tuples
         for val in product_count.values():
             if val > 1:
-                count += (val * (val - 1)) * 4  # Each pair can be arranged in 4 ways
+                count += (val * (val - 1)) * 4
         
         return count
