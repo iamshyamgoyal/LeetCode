c2eaf81fa0f70827ac801029bd46366c90d93927
Shyam Agrawal <105172277+iamshyamgoyal@users.noreply.github.com>
2025-02-26 05:39:27 +0530

    Time: 47 ms (81.46%), Space: 28.6 MB (34.63%) - LeetHub

--- 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.py
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.py
@@ -1,12 +1,26 @@
+# class Solution:
+#   def maxAbsoluteSum(self, nums):
+#     ans = -math.inf
+#     maxSum = 0
+#     minSum = 0
+
+#     for num in nums:
+#       maxSum = max(num, maxSum + num)
+#       minSum = min(num, minSum + num)
+#       ans = max(ans, maxSum, -minSum)
+
+#     return ans
+
+
 class Solution:
   def maxAbsoluteSum(self, nums):
-    ans = -math.inf
-    maxSum = 0
-    minSum = 0
+    summ = 0
+    maxPrefix = 0
+    minPrefix = 0
 
     for num in nums:
-      maxSum = max(num, maxSum + num)
-      minSum = min(num, minSum + num)
-      ans = max(ans, maxSum, -minSum)
+      summ += num
+      maxPrefix = max(maxPrefix, summ)
+      minPrefix = min(minPrefix, summ)
 
-    return ans
+    return maxPrefix - minPrefix
